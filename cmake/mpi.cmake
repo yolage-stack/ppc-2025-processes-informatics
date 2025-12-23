cfe8d9cdbbd62d259f4be0982828a39fc02c25ff
yolage-stack <andr24367@mail.ru>
2025-12-23 16:09:25 +0300

    fixed clang-format

--- cmake/mpi.cmake
+++ cmake/mpi.cmake
@@ -1,8 +1,3 @@
-find_package(MPI REQUIRED COMPONENTS CXX)
-if(NOT MPI_FOUND)
-  message(FATAL_ERROR "MPI NOT FOUND")
-endif()
+find_package(MPI REQUIRED COMPONENTS CXX) if (NOT MPI_FOUND) message(FATAL_ERROR "MPI NOT FOUND") endif()
 
-function(ppc_link_mpi exec_func_lib)
-  target_link_libraries(${exec_func_lib} PUBLIC MPI::MPI_CXX)
-endfunction()
+    function(ppc_link_mpi exec_func_lib) target_link_libraries(${exec_func_lib} PUBLIC MPI::MPI_CXX) endfunction()
